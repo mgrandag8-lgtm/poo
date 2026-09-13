46e16051406e32a13dcdf83d82213b40c3ff6044
Michael <mgrandag8@unemi.edu.ec>
2026-09-13 15:54:11 -0500

    modified:   semana1-2/grados.py

--- semana1-2/grados.py
+++ semana1-2/grados.py
@@ -8,5 +8,3 @@ class Grados:
 grados_C = float(input("ingrese la temperatura en grados celsius:"))
 grados = Grados(grados_C)
 print(f"{grados_C} grados celsius son {grados.celsius_to_fahrenheit()} grados fahrenheit")
-
-        
